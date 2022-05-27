d11ebabe47382e8c8af22c9125b7822173de1d82
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-05-27 21:30:43 +0530

    Create 876. Middle of the Linked List.java

---
+++ LinkedList/Easy/Easy/876. Middle of the Linked List.java
@@ -0,0 +1,20 @@
+Problem Link:- https://leetcode.com/problems/middle-of-the-linked-list/
+
+
+
+
+         **************CODE****************
+         
+           
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+© 2022 GitHub, Inc.
