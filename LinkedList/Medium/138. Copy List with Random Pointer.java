5ab95639f60df280537a4373c666e0f75989df79
Rahul Maurya <93531189+rahul123gautam@users.noreply.github.com>
2022-05-29 06:39:42 +0530

    Create 138. Copy List with Random Pointer.java

---
+++ LinkedList/Medium/138. Copy List with Random Pointer.java
@@ -0,0 +1,18 @@
+Problem Link:- https://leetcode.com/problems/copy-list-with-random-pointer/
+
+
+
+                *********************CODE*************************
+                
+                  
+class Solution {
+    Map<Node,Node> map = new HashMap<>();
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node temp = new Node(head.val);
+        map.put(head,temp);
+        temp.next = copyRandomList(head.next);
+        temp.random = map.get(head.random);
+        return temp;
+    }
+}
